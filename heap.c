a1356c09dcfec9fa0a9d5371731c9065c8c8f84b
akirner1 <akirner@pugetsound.edu>
2024-01-30 06:59:59 +0000

    implemented heapsort and main that runs heapsort on user created employees sorted by salary

--- heap.c
+++ heap.c
@@ -10,6 +10,59 @@
 #include "heap.h"
 
 /**
+ * Determines the index of the left child.
+ *
+ * @param	i	index of the parent node
+ * @param	n	size of the array
+ * @returns	the index of the left child
+ * @returns -1 if the index is out of the array's bounds
+ */
+int getLeftChildIndex(int i, int n){
+	int childIndex = 2 * (i + 1) - 1;
+	if(childIndex >= n){
+		return -1;
+	}else{
+		return childIndex;
+	}
+}
+
+
+/**
+ * Determines the index of the right child.
+ *
+ * @param	i	index of the parent node
+ * @param	n	size of the array
+ * @returns	the index of the right child
+ * @returns -1 if the index is out of the array's bounds
+ */
+int getRightChildIndex(int i, int n){
+	int childIndex = 2 * (i + 1);
+	if(childIndex >= n){
+		return -1;
+	}else{
+		return childIndex;
+	}
+}
+
+/**
+ * Given 2 indexes, returns the index that contains the smaller value
+ * @param *employees a pointer to the array of employees
+ * @param i1 the index of An Employee
+ * @param i2 the index Another Employee to compare to
+ * @returns the index of the employee with a lower salary
+ */
+int getSmallerIndex(Employee *employees, int i1, int i2){
+	int salary1 = employees[i1].salary;
+	int salary2 = employees[i2].salary;
+	if(salary1 < salary2){
+		return i1;
+	}else{
+		return i2;
+	}
+}
+
+
+/**
  * Sorts a list of n employees in descending order
  *
  * @param	*A	Pointer to the list of employees
@@ -18,11 +71,19 @@
 void heapSort(Employee *A, int n)
 {
 	// TODO - BuildHeap on the heap
+	buildHeap(A, n);
 
 	// TODO - while n > 0:
 	// TODO - swap A[n-1] with A[0], since A[0] is the smallest number.
 	// TODO - A[n-1] now sorted in place, so decrement n
 	// TODO - Heapify the elements from A[0] up to A[n-1] (which leaves the newly sorted element alone)
+	while(n > 0){
+		swap(&A[n-1], &A[0]);
+		n--;
+		for(int i = 0; i < n; i++){
+			heapify(A, i, n);
+		}
+	}
 }
 
 /**
@@ -36,6 +97,9 @@ void heapSort(Employee *A, int n)
 void buildHeap(Employee *A, int n)
 {
 	// TODO - heapify() every element from A[n/2] down-to A[0]
+	for(int i = n/2; i >= 0; i--){
+		heapify(A, i, n);
+	}
 }
 
 /**
@@ -48,16 +112,39 @@ void buildHeap(Employee *A, int n)
  */
 void heapify(Employee *A, int i, int n)
 {
+	if(i >= n){
+		//i is not in range, no more swaps needed
+		return;
+	}
 	// TODO - get index of left child of element i
 	// TODO - get index of right child of element i
+	int leftChildIndex = getLeftChildIndex(i, n);
+	int rightChildIndex = getRightChildIndex(i, n);
 
 	// TODO - determine which child has a smaller salary. We'll call the index of this
 	//		element: "smaller"
+	int smaller;
+	if(leftChildIndex == rightChildIndex && leftChildIndex == -1){
+		//child node, no swaps needed;
+		return;
+	}
+	if(leftChildIndex == -1){
+		smaller = rightChildIndex;
+	}else if(rightChildIndex == -1){
+		smaller = leftChildIndex;
+	}else{
+		smaller = getSmallerIndex(A, leftChildIndex, rightChildIndex);
+	}
 
 	// TODO - recursively check if the salary at A[i] > the salary at A[smaller]. If it is, swap the two.
 	//			Then recursively heapify A[smaller].
+	if(A[i].salary > A[smaller].salary){
+		swap(&A[i], &A[smaller]);
+		heapify(A, smaller, n);
+	}
 	// TODO - Continue recursion as long as i is within range AND either right_child and left_child are still within range.
 }
+
 
 /**
  * Swaps the locations of two Employees
@@ -66,7 +153,9 @@ void heapify(Employee *A, int i, int n)
  */
 void swap(Employee *e1, Employee *e2)
 {
-	// TODO
+	Employee temp = *e1;
+	*e1 = *e2;
+	*e2 = temp;
 }
 
 /**
@@ -76,5 +165,8 @@ void swap(Employee *e1, Employee *e2)
  */
 void printList(Employee *A, int n)
 {
-	// TODO
+	for(int i = 0; i < n; i++){
+		printf("[id=%s sal=%d] ", A[i].name, A[i].salary);
+	}
+	printf("\n");
 }
--- main.c
+++ main.c
@@ -5,6 +5,44 @@
 int main(int argc, char *argv[])
 {
 	//TODO
-	
+	int MAX_EMPLOYEES = 5;
+	Employee employees[MAX_EMPLOYEES];
+	int validUser = 0;
+	int ch;
+	int index;
+	int i;
+	for(i = 0; i < MAX_EMPLOYEES && validUser < 2; i++){
+		validUser = 0;
+		while(!validUser){
+			struct Employee tempEmployee;
+			printf("Please enter in the name of employee #%d: ", i+1);
+			index = 0;
+			while( (ch = getchar()) != '\n' ) {
+				if (index < sizeof(tempEmployee.name) - 1){
+					tempEmployee.name[index] = ch;
+					index++;
+				}
+				
+			}
+			tempEmployee.name[index] = '\0';
+
+			printf("Please enter in the salary for employee %s: ", tempEmployee.name);
+			scanf("%d", &tempEmployee.salary);
+			getchar(); //scanf doen't remove the '\n' character from the input stream, so we do it here
+			employees[i] = tempEmployee;
+			printf("You created an employee with name: %s, and salary %d.\n", tempEmployee.name, tempEmployee.salary);
+			printf("Is this correct? type '0' to recreate employee, '1' to continue, '2' to exit employee creation: ");
+			scanf("%d", &validUser);
+			getchar();
+		}
+		
+	}
+	if(i == MAX_EMPLOYEES){
+		printf("Maximum employees reached. Exiting employee creation\n");
+	}
+	int numEmployees = i;
+	heapSort(employees, numEmployees);
+	printList(employees, numEmployees);
+
 	return 0;
 }
